788bb6c893774d7c7d16ba002c285c3778f1e0d4
Chris Catchings <chris@ccatchings.com>
2024-04-15 16:36:10 +0200

    First commit

---
+++ runkeycloak.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp /ssefs/server.keystore /opt/keycloak/conf/.
+/opt/keycloak/bin/kc.sh start --https-key-store-file=/opt/keycloak/conf/server.keystore --https-key-store-password=$KS_PW --import-realm
