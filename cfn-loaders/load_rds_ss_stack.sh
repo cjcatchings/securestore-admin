e5dd1ad85f33304c0ff950d32678754149a66e47
Chris Catchings <chris@ccatchings.com>
2024-05-01 19:59:28 +0200

    Moved IAM policies to separate JSON files
    Cleans up the CloudFormation templates and allows the necessary policies to be loaded separately.

--- cfn-loaders/load_rds_ss_stack.sh
+++ cfn-loaders/load_rds_ss_stack.sh
@@ -10,7 +10,7 @@ EXISTING_STACK=$(aws cloudformation list-stacks \
     --output text \
     --query "StackSummaries[?StackName=='Eks-Rds-SS-Stack-CF'].StackName")
 if ! [ -n ${EXISTING_STACK} ]; then
-    echo "'${EXISTING_STACK}' already exists."
+    echo "'${EXISTING_STACK}' already exists.  In the future let's call update-stack..."
     exit 0
 fi
 aws cloudformation deploy \
